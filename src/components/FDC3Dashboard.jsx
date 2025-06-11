456e715dade05e8bd8f9c0e0ded97c6ca1ef3ee4
yadnyeshkolte <yadnyeshkolte@gmail.com>
2025-06-11 15:38:59 +0530

    working css

--- src/components/FDC3Dashboard.jsx
+++ src/components/FDC3Dashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, Filler } from 'chart.js';
 import { Line } from 'react-chartjs-2';
 
